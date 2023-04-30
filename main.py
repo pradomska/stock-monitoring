2de12b5a7e6a8649f3f9f1e314d653673d7657f5
Paulina Radomska <paulinaradomska10@wp.pl>
2023-04-30 22:07:19 +0200

    top 3 news about my stock company

--- main.py
+++ main.py
@@ -7,20 +7,20 @@ STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 STOCK_API_KEY = "3ZGZQGDVSC8BOLLM"
+NEWS_API_KEY = "370ba7b785404b09876fdabc3f7b444d"
 
 stock_params = {
     "function": "TIME_SERIES_DAILY_ADJUSTED",
     "symbol": STOCK_NAME,
     "apikey": STOCK_API_KEY,
 }
-response = requests.get(STOCK_ENDPOINT, params=stock_params)
-data = response.json()["Time Series (Daily)"]
-data_list = [value for (key, value) in data.items()]
-yesterday_data = data_list[0]
+stock_response = requests.get(STOCK_ENDPOINT, params=stock_params).json()["Time Series (Daily)"]
+data = [value for (key, value) in stock_response.items()]
+yesterday_data = data[0]
 yesterday_closing_price = yesterday_data["4. close"]
 # print(yesterday_closing_price)
 
-day_before_yesterday_data = data_list[1]
+day_before_yesterday_data = data[1]
 day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
 # print(day_before_yesterday_closing_price)
 
@@ -30,7 +30,13 @@ difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_clo
 diff_percent = (difference / float(yesterday_closing_price)) * 100
 # print(diff_percent)
 if diff_percent > 2.5:
-    print("Get News")
+    news_params = {
+        "qInTitle": COMPANY_NAME,
+        "apiKey": NEWS_API_KEY,
+    }
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params).json()["articles"]
+    three_articles = news_response[:3]
+    print(three_articles)
 
 ## STEP 3: Use https://www.twilio.com
 # Send a seperate message with the percentage change and each article's title and description to your phone number. 
